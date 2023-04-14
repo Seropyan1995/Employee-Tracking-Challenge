3276f6436eeac570e8c122a1a28b123b972aa470
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-04-14 10:57:20 -0400

    changes to index.js

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '123456',
     database: 'company_db',
+  });
 
+  db.connect(err => {
+    if(err){
+      console.log(err)
+    }else{
+      console.log('Connected to DB');
+    }
   });
 
 
-afterConnection
-  afterConnection = () => {
-    console.log("***********************************")
-    console.log("*    ___                          *")
-    console.log("*   |___                          *")
-    console.log("*   |___                          *")
-    console.log("***********************************")
 
+      console.log('Schema sourced successfully');
+    // C:\Users\shiny\Employee Tracking Challenge\db\schema.sql
+   jumbotronMessage= () => {
+    if(db === true){
+      schema
+      seedsSQL
+    }
+      console.log('connection worked')
+      console.log("***********************************");
+      console.log("*                                 *");
+      console.log("*      TO DO: WELCOME BOARD       *");
+      console.log("*                                 *");
+      console.log("***********************************");
   };
 
-//  https://github.com/nicolewallace09/employee-tracker/blob/master/db/seeds.sql
+
