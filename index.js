2f595f931ac415bc2f4325bcaafa0b9891aa41b6
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-04-14 09:53:44 -0400

    removed and restructed files, testing out solutions

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+const mysql = require('mysql2');
+const cTable = require('console.table');
 const inquirer = require('inquirer');
-const { db,
-     express,
-      app,
-       mysql,
-        PORT,
-        displayTable} = require("./server");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'company_db',
+
+  });
 
 
+afterConnection
+  afterConnection = () => {
+    console.log("***********************************")
+    console.log("*    ___                          *")
+    console.log("*   |___                          *")
+    console.log("*   |___                          *")
+    console.log("***********************************")
 
+  };
 
 //  https://github.com/nicolewallace09/employee-tracker/blob/master/db/seeds.sql
